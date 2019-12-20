bb05f1aec3d5b518e14685bc310289bc2c8a8952
Andrew Ackerman <andrew.ackers@gmail.com>
2019-12-20 10:18:03 -0800

    MVP

--- client/src/components/BubblePage.js
+++ client/src/components/BubblePage.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import axios from "axios";
+import AxiosAuth from "../AxiosAuth";
 
 import Bubbles from "./Bubbles";
 import ColorList from "./ColorList";
@@ -9,10 +10,27 @@ const BubblePage = () => {
   // fetch your colors data from the server when the component mounts
   // set that data to the colorList state property
 
+  useEffect(() => {
+    getData();
+  }, []);
+
+  const getData = () => {
+    AxiosAuth()
+      .get("/colors")
+      .then(res => {
+        setColorList(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  };
+
+  console.log(colorList);
+
   return (
     <>
-      <ColorList colors={colorList} updateColors={setColorList} />
       <Bubbles colors={colorList} />
+      <ColorList colors={colorList} updateColors={setColorList} />
     </>
   );
 };
